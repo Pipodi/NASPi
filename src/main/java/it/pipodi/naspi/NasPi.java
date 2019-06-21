7af248ed4e9c6b8b6414d44136597a0fd5592a92
Alex Collini <pipodi93@gmail.com>
2019-06-21 15:33:30 +0200

    Add Movie streaming endpoint

--- src/main/java/it/pipodi/naspi/NasPi.java
+++ src/main/java/it/pipodi/naspi/NasPi.java
@@ -2,13 +2,11 @@ package it.pipodi.naspi;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NasPi implements ApplicationRunner {
+public class NasPi {
 
 	private static Logger logger = LoggerFactory.getLogger(NasPi.class);
 
@@ -17,8 +15,5 @@ public class NasPi implements ApplicationRunner {
 		SpringApplication.run(NasPi.class, args);
 	}
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
 
-	}
 }
--- src/main/java/it/pipodi/naspi/endpoints/MoviesEndpoint.java
+++ src/main/java/it/pipodi/naspi/endpoints/MoviesEndpoint.java
@@ -1,8 +1,10 @@
 package it.pipodi.naspi.endpoints;
 
+import it.pipodi.naspi.config.NasPiConfig;
 import it.pipodi.naspi.model.UploadResponse;
 import it.pipodi.naspi.model.wrappers.Movies;
 import it.pipodi.naspi.orchestration.MoviesOrchestration;
+import it.pipodi.naspi.services.MultipartFileSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 @RestController
@@ -23,6 +28,9 @@ public class MoviesEndpoint {
 
 	@Autowired
 	private MoviesOrchestration moviesOrchestration;
+
+	@Autowired
+	private NasPiConfig config;
 
 	private static final Logger logger = LoggerFactory.getLogger(MoviesEndpoint.class);
 
@@ -76,5 +84,28 @@ public class MoviesEndpoint {
 	public ResponseEntity<UploadResponse> uploadFile(@RequestParam("file") MultipartFile file) {
 		logger.debug("uploadFile() method called");
 		return ResponseEntity.ok(this.moviesOrchestration.uploadFile(file));
+	}
+
+	/**
+	 * GET endpoint that streams a movie saved on the filesystem
+	 *
+	 * @param request  httpservletrequest
+	 * @param response httpservletresponse
+	 * @param filename the name of the file, including the extension
+	 */
+	@RequestMapping(value = "/stream/{filename}", method = RequestMethod.GET)
+	public void stream(HttpServletRequest request,
+	                   HttpServletResponse response, @PathVariable String filename) {
+		String filepath = String.format("%s/Movies/%s", config.getRootFolder(), filename);
+		Path path = Paths.get(filepath);
+		try {
+			MultipartFileSender.fromPath(path)
+					.with(request)
+					.with(response)
+					.serveResource();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
 	}
 }
